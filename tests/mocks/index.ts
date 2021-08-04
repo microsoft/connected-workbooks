8476dca9967cdbd986ef83432befca51e733b184
sbeih <v-ahmadsbeih@microsoft.com>
2021-08-04 15:33:50 +0300

    Adding mashupDocumentParser test

---
+++ tests/mocks/index.ts
@@ -0,0 +1,2 @@
+export { default as simpleQuery } from "./simpleQueryMock";
+export { default as section1mMock } from "./section1mSimpleQueryMock";
---
+++ tests/mocks/simpleQueryMock.ts
@@ -0,0 +1,6 @@
+export default `
+let
+    Source = Folder.Files("C:\\Users\\user1\\Desktop\\test")
+in
+    Source
+`;
