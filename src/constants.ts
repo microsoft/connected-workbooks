2b252f3dcf8dc24ccd89e36510af8c95fa478ece
Ahmad Sbeih (ASAL Technologies Ltd) <v-ahmadsbeih@microsoft.com>
2021-12-27 10:59:17 +0200

    adding ConnectedWorkbook xml

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export const pqCustomXmlPath = "customXml/item1.xml";
+export const pqCustomXmlPath = "customXml/item2.xml";
 export const connectionsXmlPath = "xl/connections.xml";
 export const queryTablesPath = "xl/queryTables/";
 export const pivotCachesPath = "xl/pivotCache/";
