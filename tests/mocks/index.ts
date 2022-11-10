b7d129832b80e72655a6c01559112576aaad2ab7
shanialbeck <t-shanim@microsoft.com>
2022-11-10 06:35:22 -0600

    test for mashup document - naming query

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -1,3 +1,3 @@
-export { simpleQueryMock, section1mSimpleQueryMock, section1mBlankQueryMock } from "./section1mSimpleQueryMock";
-export { pqEmptySingleQueryBase64, item1Path, item2Path } from "./PqMock";
+export { simpleQueryMock, section1mSimpleQueryMock, section1mBlankQueryMock, section1mNewQueryNameSimpleMock, section1mNewQueryNameBlankMock } from "./section1mSimpleQueryMock";
+export { pqEmptySingleQueryBase64, item1Path, item2Path, relationshipInfo } from "./PqMock";
 export { connectedWorkbookXmlMock } from "./xmlMocks";
