8f468a6e76c3743abf804f0d34a0f3a619823263
shanialbeck <t-shanim@microsoft.com>
2022-11-10 03:14:45 -0600

    test query name is in  sharedstringsxml

--- tests/workbookManager.test.ts
+++ tests/workbookManager.test.ts
@@ -1,6 +1,6 @@
 import workbookTemplate from "../src/workbookTemplate";
 import { WorkbookManager }  from "../src/workbookManager";
-import { connectionsXmlPath } from "../src/constants";
+import { connectionsXmlPath, sharedStringsXmlPath } from "../src/constants";
 import JSZip from "jszip";
 
 describe("Workbook Manager tests", () => {
@@ -14,5 +14,13 @@ describe("Workbook Manager tests", () => {
         const hasQuery1 = connectionsXmlString?.includes("Query1");
         expect(hasQueryNewName).toBeTruthy();
         expect(hasQuery1).toBeFalsy;
+    })
+
+    test("SharedStrings XML contain new query name", async () => {
+        const defaultZipFile = await JSZip.loadAsync(workbookTemplate.SIMPLE_QUERY_WORKBOOK_TEMPLATE, { base64: true }) 
+        await workbookManager.updateSingleQueryAttributes(defaultZipFile, "newQueryName", true);
+        const sharedStringsXmlString = await defaultZipFile.file(sharedStringsXmlPath)?.async("text");
+        const hasQueryNewName = sharedStringsXmlString?.includes("newQueryName");
+        expect(hasQueryNewName).toBeTruthy();
     })
 });
