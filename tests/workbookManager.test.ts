9b5632be9ba8950780f421a4dc3e95a6bca1d8c8
shanialbeck <t-shanim@microsoft.com>
2022-11-10 02:45:44 -0600

    tests for connectionXML naming query

---
+++ tests/workbookManager.test.ts
@@ -0,0 +1,18 @@
+import workbookTemplate from "../src/workbookTemplate";
+import { WorkbookManager }  from "../src/workbookManager";
+import { connectionsXmlPath } from "../src/constants";
+import JSZip from "jszip";
+
+describe("Workbook Manager tests", () => {
+    const workbookManager = new WorkbookManager() as any;
+
+    test("Connection XML attributes contain new query name", async () => {
+        const defaultZipFile = await JSZip.loadAsync(workbookTemplate.SIMPLE_QUERY_WORKBOOK_TEMPLATE, { base64: true });
+        await workbookManager.updateSingleQueryAttributes(defaultZipFile, "newQueryName", true);
+        const connectionsXmlString = await defaultZipFile.file(connectionsXmlPath)?.async("text");
+        const hasQueryNewName = connectionsXmlString?.includes("newQueryName");
+        const hasQuery1 = connectionsXmlString?.includes("Query1");
+        expect(hasQueryNewName).toBeTruthy();
+        expect(hasQuery1).toBeFalsy;
+    })
+});
