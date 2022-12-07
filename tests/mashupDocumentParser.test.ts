949c4a74b807ce0f0a04789c366922b1ead25d63
shanialbeck <t-shanim@microsoft.com>
2022-12-07 04:19:10 -0600

    add mashupdoc tests

--- tests/mashupDocumentParser.test.ts
+++ tests/mashupDocumentParser.test.ts
@@ -7,22 +7,22 @@ import WorkbookTemplate from "../src/workbookTemplate";
 import { section1mPath } from "../src/constants";
 describe("Mashup Document Parser tests", () => {
     test("ReplaceSingleQuery test", async () => {
-        // const mashupHandler = new MashupHandler();
+        const mashupHandler = new MashupHandler();
 
-        // const defaultZipFile = await JSZip.loadAsync(WorkbookTemplate.SIMPLE_QUERY_WORKBOOK_TEMPLATE, { base64: true });
-        // const originalBase64Str = await pqUtils.getBase64(defaultZipFile);
+        const defaultZipFile = await JSZip.loadAsync(WorkbookTemplate.SIMPLE_QUERY_WORKBOOK_TEMPLATE, { base64: true });
+        const originalBase64Str = await pqUtils.getBase64(defaultZipFile);
 
-        // const replacedQueryBase64Str = await mashupHandler.ReplaceSingleQuery(originalBase64Str, simpleQueryMock);
+        const replacedQueryBase64Str = await mashupHandler.ReplaceSingleQuery(originalBase64Str, simpleQueryMock);
 
-        // const buffer = base64.toByteArray(replacedQueryBase64Str).buffer;
-        // const mashupArray = new arrayUtils.ArrayReader(buffer);
-        // const startArray = mashupArray.getBytes(4);
-        // const packageSize = mashupArray.getInt32();
-        // const packageOPC = mashupArray.getBytes(packageSize);
+        const buffer = base64.toByteArray(replacedQueryBase64Str).buffer;
+        const mashupArray = new arrayUtils.ArrayReader(buffer);
+        const startArray = mashupArray.getBytes(4);
+        const packageSize = mashupArray.getInt32();
+        const packageOPC = mashupArray.getBytes(packageSize);
 
-        // const zip = await JSZip.loadAsync(packageOPC);
-        // const section1m = await zip.file(section1mPath)?.async("text");
+        const zip = await JSZip.loadAsync(packageOPC);
+        const section1m = await zip.file(section1mPath)?.async("text");
 
-        // expect(section1m.replace(/ /g, "")).toEqual(section1mSimpleQueryMock.replace(/ /g, ""));
+        expect(section1m.replace(/ /g, "")).toEqual(section1mSimpleQueryMock.replace(/ /g, ""));
     });
 });
