660287810115d6aa472d821cb45e08a82aae6ab8
shanialbeck <t-shanim@microsoft.com>
2023-02-28 12:02:42 +0200

    add GridParser functionality

---
+++ src/GridParser.ts
@@ -0,0 +1,55 @@
+import { ColumnMetadata, dataTypes, Grid, TableData } from "./types";
+
+export default class GridParser {
+    public parseToTableData(initialDataGrid: Grid): TableData | undefined {
+        if (!initialDataGrid) {
+            return undefined;
+        }
+        this.validateGridHeader(initialDataGrid);
+        const data = this.parseGridData(initialDataGrid, initialDataGrid.Header);
+        return { columnMetadata : initialDataGrid.Header, data: data };
+    }
+
+    private parseGridData(initialDataGrid: Grid, columnMetadata: ColumnMetadata[]) {
+        const gridData = initialDataGrid.GridData;
+        if (!gridData) {
+            throw new Error("Invalid JSON file, grid data is missing");
+        }
+        const tableData: string[][] = [];
+        for (const rowData of gridData) {
+            const row: string[] = [];
+            var colIndex = 0;
+            for (const prop in rowData) {
+                const dataType = columnMetadata[colIndex].type;
+                const cellValue = rowData[prop];
+                if (dataType == dataTypes.number) {
+                    if (isNaN(Number(cellValue))) {
+                        throw new Error("Invalid cell value in number column");
+                    }
+                }
+                if (dataType == dataTypes.boolean) {
+                    if (cellValue != "1" && cellValue != "0") {
+                        throw new Error("Invalid cell value in boolean column");
+                    }
+                }
+                row.push(rowData[prop].toString());
+                colIndex++;
+            }
+            tableData.push(row);
+        }
+        return tableData;
+    }
+
+    private validateGridHeader(data: Grid) {
+        const headerData = data.Header;
+        if (!headerData) {
+            throw new Error("Invalid JSON file, header is missing");
+        }
+        for (const prop in headerData) {
+            if (!(headerData[prop].type in dataTypes)) { 
+                throw new Error("Invalid JSON file, invalid data type");
+        }
+    }
+}
+}
+
