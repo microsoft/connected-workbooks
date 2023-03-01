8de7b096290bc3c8605e026b75da7ad02d9c027b
shanialbeck <t-shanim@microsoft.com>
2023-03-01 16:53:30 +0200

    add connection only to metadata

--- src/mashupDocumentParser.ts
+++ src/mashupDocumentParser.ts
@@ -19,6 +19,15 @@ export default class MashupHandler {
         const newMashup = arrayUtils.concatArrays(version, packageSizeBuffer, newPackageBuffer, permissionsSizeBuffer, permissions, metadataSizeBuffer, newMetadataBuffer, endBuffer);
         return base64.fromByteArray(newMashup);
     }
+    async AddConnectionOnlyQuery(base64Str: string, queryName: string): Promise<string> {
+        var { version, packageOPC, permissionsSize, permissions, metadata, endBuffer } = this.getPackageComponents(base64Str);
+        const packageSizeBuffer = arrayUtils.getInt32Buffer(packageOPC.byteLength);
+        const permissionsSizeBuffer = arrayUtils.getInt32Buffer(permissionsSize);
+        const newMetadataBuffer = this.addConnectionOnlyQueryMetadata(metadata, queryName);
+        const metadataSizeBuffer = arrayUtils.getInt32Buffer(newMetadataBuffer.byteLength);
+        const newMashup = arrayUtils.concatArrays(version, packageSizeBuffer, packageOPC, permissionsSizeBuffer, permissions, metadataSizeBuffer, newMetadataBuffer, endBuffer);
+        return base64.fromByteArray(newMashup);
+    }
 
     private getPackageComponents(base64Str: string) {
         const buffer = base64.toByteArray(base64Str).buffer;
@@ -143,4 +152,103 @@ export default class MashupHandler {
         const newMetadataArray = arrayUtils.concatArrays(metadataVersion, newMetadataXmlSize, newMetadataXml, endBuffer);
         return newMetadataArray;
     };
+
+    private addConnectionOnlyQueryMetadata = (metadataArray: Uint8Array, queryName: string) => {
+        //extract metadataXml
+        const mashupArray = new arrayUtils.ArrayReader(metadataArray.buffer);
+        const metadataVersion = mashupArray.getBytes(4);
+        const metadataXmlSize = mashupArray.getInt32();
+        const metadataXml = mashupArray.getBytes(metadataXmlSize);
+        const endBuffer = mashupArray.getBytes();
+
+        //parse metadataXml
+        const metadataString = uintToString(metadataXml);
+        const newMetadataString = this.updateConnectionOnlyMetadataStr(metadataString, queryName);
+
+        const encoder = new TextEncoder();
+        const newMetadataXml = encoder.encode(newMetadataString);
+        const newMetadataXmlSize = arrayUtils.getInt32Buffer(newMetadataXml.byteLength);
+        const newMetadataArray = arrayUtils.concatArrays(
+            metadataVersion,
+            newMetadataXmlSize,
+            newMetadataXml,
+            endBuffer
+        );
+        return newMetadataArray;
+    };
+
+    private updateConnectionOnlyMetadataStr = (metadataString: string, queryName: string) => {
+        const parser = new DOMParser();
+        const metadataDoc = parser.parseFromString(metadataString, "text/xml");     
+        this.createStableEntriesItem(metadataDoc, queryName);
+        this.createSourceItem(metadataDoc, queryName);
+
+        const serializer = new XMLSerializer();
+        const newMetadataString = serializer.serializeToString(metadataDoc);
+        return newMetadataString;
+    };
+
+    private createSourceItem = (metadataDoc: Document, queryName: string) => {
+        const items = metadataDoc.getElementsByTagName("Items")[0];
+        const newItemSource = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "Item");
+        const newItemLocation = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "ItemLocation");
+        const newItemType = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "ItemType");
+        newItemType.textContent = "Formula";
+        const newItemPath = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "ItemPath");
+        newItemPath.textContent = `Section1/${queryName}/Source`;
+        newItemLocation.appendChild(newItemType);
+        newItemLocation.appendChild(newItemPath);
+        newItemSource.appendChild(newItemLocation);
+        items.appendChild(newItemSource);
+    };
+
+    private createStableEntriesItem = (metadataDoc: Document, queryName: string) => {
+        const items = metadataDoc.getElementsByTagName("Items")[0];
+        const newItem = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "Item");
+        const newItemLocation = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "ItemLocation");
+        const newItemType = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "ItemType");
+        newItemType.textContent = "Formula";
+        const newItemPath = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "ItemPath");
+        newItemPath.textContent = `Section1/${queryName}`;
+        newItemLocation.appendChild(newItemType);
+        newItemLocation.appendChild(newItemPath); 
+        newItem.appendChild(newItemLocation);
+        items.append(newItem);
+        let stableEntries = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "StableEntries");
+        stableEntries = this.createConnectionOnlyEntries(metadataDoc, stableEntries);
+        newItem.appendChild(stableEntries);
+    };
+
+    private createConnectionOnlyEntries = (metadataDoc: Document, stableEntries: Element) => {
+        const IsPrivate = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "Entry");
+        IsPrivate.setAttribute("Type", "IsPrivate");
+        IsPrivate.setAttribute("Value", "l0");
+        stableEntries.appendChild(IsPrivate);
+        const FillEnabled = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "Entry");
+        FillEnabled.setAttribute("Type", "FillEnabled");
+        FillEnabled.setAttribute("Value", "l0");
+        stableEntries.appendChild(FillEnabled);
+        const FillObjectType = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "Entry");
+        FillObjectType.setAttribute("Type", "FillObjectType");
+        FillObjectType.setAttribute("Value", "sConnectionOnly");
+        stableEntries.appendChild(FillObjectType);
+        const FillToDataModelEnabled = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "Entry");
+        FillToDataModelEnabled.setAttribute("Type", "FillToDataModelEnabled");
+        FillToDataModelEnabled.setAttribute("Value", "l0");
+        const FillLastUpdated = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "Entry");
+        FillLastUpdated.setAttribute("Type", "FillLastUpdated");
+        const nowTime = new Date().toISOString();
+        FillLastUpdated.setAttribute("Value", ("d" + nowTime).replace(/Z/, "0000Z"));
+        stableEntries.appendChild(FillLastUpdated);
+        const ResultType = metadataDoc.createElementNS(metadataDoc.documentElement.namespaceURI, "Entry");
+        ResultType.setAttribute("Type", "ResultType");
+        ResultType.setAttribute("Value", "sTable");
+        stableEntries.appendChild(ResultType);
+        return stableEntries;
+    };
+}
+
+function uintToString(uintArray: Uint8Array) {
+    var encodedString = new TextDecoder("utf-8").decode(uintArray);
+    return encodedString;
 }
