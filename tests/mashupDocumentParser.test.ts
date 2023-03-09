73156fa3ce7eb6ea6696a248954aca64b1361ca7
shanialbeck <t-shanim@microsoft.com>
2023-03-09 10:03:01 +0200

    add metadata testing

--- tests/mashupDocumentParser.test.ts
+++ tests/mashupDocumentParser.test.ts
@@ -1,7 +1,7 @@
 import { TextDecoder, TextEncoder } from 'util';
 import MashupHandler from "../src/mashupDocumentParser";
 import { arrayUtils, pqUtils } from "../src/utils";
-import { simpleQueryMock, section1mNewQueryNameSimpleMock, pqMetadataXmlMockPart1, pqMetadataXmlMockPart2 } from "./mocks";
+import { simpleQueryMock, section1mNewQueryNameSimpleMock, pqMetadataXmlMockPart1, pqMetadataXmlMockPart2, pqMetadataXmlMock } from "./mocks";
 import base64 from "base64-js";
 import JSZip from "jszip";
 import WorkbookTemplate from "../src/workbookTemplate";
@@ -46,4 +46,80 @@ describe("Mashup Document Parser tests", () => {
             expect(metadataString.replace(/ /g, "")).toContain(pqMetadataXmlMockPart2.replace(/ /g, ""));
         }
     })
+
+
+    test("Create new connection only stableEntries metadata item", async () => {
+        const defaultZipFile = await JSZip.loadAsync(WorkbookTemplate.SIMPLE_QUERY_WORKBOOK_TEMPLATE, { base64: true });
+        const originalBase64Str = await pqUtils.getBase64(defaultZipFile); 
+        if  (originalBase64Str) {
+            const handler = new MashupHandler() as any;
+            const { metadata } = handler.getPackageComponents(originalBase64Str);
+            const newMetadataArray = handler.editSingleQueryMetadata(metadata, {queryName: "Query1"});
+            const util = require('util');
+            const metadataString = (new util.TextDecoder("utf-8").decode(newMetadataArray));
+            const metadataDoc = new DOMParser().parseFromString(metadataString, "text/xml");  
+            const stableEntriesItem = handler.createStableEntriesItem(metadataDoc, "Query2");
+            compareConnectionOnlyStableEntries(stableEntriesItem);
+            compareConnectionOnlyItemLocation(stableEntriesItem, "Query2");
+        }
+    })
+
+    test("Create new connection only src metadata item", async () => {
+        const defaultZipFile = await JSZip.loadAsync(WorkbookTemplate.SIMPLE_QUERY_WORKBOOK_TEMPLATE, { base64: true });
+        const originalBase64Str = await pqUtils.getBase64(defaultZipFile); 
+        if  (originalBase64Str) {
+            const handler = new MashupHandler() as any;
+            const { metadata } = handler.getPackageComponents(originalBase64Str);
+            const util = require('util');
+            const metadataString = (new util.TextDecoder("utf-8").decode(metadata));
+            const metadataDoc = new DOMParser().parseFromString(metadataString, "text/xml");  
+            const sourceItem = handler.createSourceItem(metadataDoc, "Query2");
+            compareConnectionOnlySrcItemLocation(sourceItem, "Query2");
+        }
+    })
+
+    test("Add connection only query to metadata", async () => {
+        const handler = new MashupHandler() as any;
+        const connectionOnlyMetadataStr = await handler.updateConnectionOnlyMetadataStr(pqMetadataXmlMock, "Query2");
+        expect((connectionOnlyMetadataStr.match(new RegExp("<Item>", "g")) || []).length).toEqual(5);
+        
+    })
+    
 });
+
+function compareConnectionOnlyItemLocation(stableEntriesItem: Element, queryName: string) {
+    const path = [...stableEntriesItem.getElementsByTagName("ItemPath")][0];
+    expect(path.textContent).toEqual(`Section1/${queryName}`);
+    const type = [...stableEntriesItem.getElementsByTagName("ItemType")][0];
+    expect(type.textContent).toEqual("Formula");
+}
+
+function compareConnectionOnlySrcItemLocation(stableEntriesItem: Element, queryName: string) {
+    const path = [...stableEntriesItem.getElementsByTagName("ItemPath")][0];
+    expect(path.textContent).toEqual(`Section1/${queryName}/Source`);
+    const type = [...stableEntriesItem.getElementsByTagName("ItemType")][0];
+    expect(type.textContent).toEqual("Formula");
+}
+
+function compareConnectionOnlyStableEntries(item: Element) {
+    const entries = [...item.getElementsByTagName("Entry")];
+    expect(entries.length).toEqual(6);
+    entries.forEach(entry => {
+        if (entry.getAttribute("type") === "IsPrivate") {
+            expect(entry.getAttribute("value")).toEqual("l0");
+            }
+        if (entry.getAttribute("type") === "FillEnabled") {
+            expect(entry.getAttribute("value")).toEqual("l0");
+            }
+        if (entry.getAttribute("type") === "FillObjectType") {
+            expect(entry.getAttribute("value")).toEqual("sConnectionOnly");
+            }
+        if (entry.getAttribute("type") === "FillToDataModelEnabled") {
+            expect(entry.getAttribute("value")).toEqual("l0");
+            }
+        if (entry.getAttribute("type") === "ResultType") {
+            expect(entry.getAttribute("value")).toEqual("sTable");
+            }
+    });
+}
+
