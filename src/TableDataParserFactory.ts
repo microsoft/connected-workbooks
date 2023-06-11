1b8a3caf301e9bceaa37a1bfbc8f681a08467549
Matan Bezen <mabezen@microsoft.com>
2023-06-11 18:37:00 +0300

    update the run time type check in the factory method

--- src/TableDataParserFactory.ts
+++ src/TableDataParserFactory.ts
@@ -3,7 +3,7 @@ import { Grid, TableDataParser } from "./types";
 
 export default class TableDataParserFactory {
  public static createParser(data: any): TableDataParser {
-    if (data as Grid && data) {
+    if (data as Grid) {
         return new GridParser();
     }
     
