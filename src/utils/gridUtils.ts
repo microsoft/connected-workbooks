42cbdcbeda3b18404898796733f53ccd43769e7d
roben <roben@microsoft.com>
2023-07-04 23:34:50 +0300

    Add grid validation (see function documentation)

--- src/utils/gridUtils.ts
+++ src/utils/gridUtils.ts
@@ -21,10 +21,54 @@ const parseToTableData = (grid: Grid): TableData => {
         },
         data: grid.data,
     };
+
+    validateGrid(mergedGrid);
     const columnNames: string[] = generateColumnNames(mergedGrid);
     const rows: string[][] = parseGridRows(mergedGrid);
 
     return { columnNames: columnNames, rows: rows };
+};
+
+/*
+ * Validates the grid, throws an error if the grid is invalid.
+ * A valid grid has:
+ * - MxN structure.
+ * - If promoteHeaders is true - has at least 1 row, and in case adjustColumnNames is false, first row is unique and non empty.
+ */
+const validateGrid = (grid: MergedGrid): void => {
+    if (!validateDataArrayDimensions(grid.data)) {
+        throw new Error("Invalid grid dimensions");
+    }
+
+    if (grid.config.promoteHeaders && grid.data.length === 0) {
+        throw new Error("Promote headers is not supported for an empty grid");
+    }
+
+    if (grid.config.promoteHeaders && grid.config.adjustColumnNames === false && !validateUniqueAndValidDataArray(grid.data[0])) {
+        throw new Error("Headers cannot be promoted without adjusting column names");
+    }
+};
+
+const validateDataArrayDimensions = (arr: unknown[][]): boolean => {
+    if (arr.length === 0) {
+        return true; // Empty array is considered valid
+    }
+    const innerLength = arr[0].length;
+
+    if (innerLength === 0) {
+        return false; // [[]] and any [] innerArr is invalid
+    }
+
+    return arr.every((innerArr) => innerArr.length === innerLength);
+};
+
+const validateUniqueAndValidDataArray = (arr: (string | number | boolean)[]): boolean => {
+    if (arr.some((element) => element === "")) {
+        return false; // Array contains empty elements
+    }
+
+    const uniqueSet = new Set(arr);
+    return uniqueSet.size === arr.length;
 };
 
 const parseGridRows = (grid: MergedGrid): string[][] => {
