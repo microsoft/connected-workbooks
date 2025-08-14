72fd6a14ebb0abe6521a69943d30bf823e462ef6
Meni Kroizer <menikroizer+microsoft@microsoft.com>
2025-08-14 13:35:19 +0300

    add UT

--- tests/xmlInnerPartsUtils.test.ts
+++ tests/xmlInnerPartsUtils.test.ts
@@ -2,8 +2,10 @@
 // Licensed under the MIT license.
 
 import { sharedStringsXmlMock, existingSharedStringsXmlMock } from "./mocks";
-import { xmlInnerPartsUtils } from "../src/utils";
+import { gridUtils, xmlInnerPartsUtils, xmlPartsUtils } from "../src/utils";
 import { describe, test, expect } from '@jest/globals';
+import JSZip from "jszip";
+import { SIMPLE_BLANK_TABLE_TEMPLATE, SIMPLE_QUERY_WORKBOOK_TEMPLATE, WORKBOOK_TEMPLATE_MOVED_TABLE } from "../src/workbookTemplate";
 
 describe("Workbook Manager tests", () => {
     const mockConnectionString = `<connections xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:xr16="http://schemas.microsoft.com/office/spreadsheetml/2017/revision16" mc:Ignorable="xr16">
@@ -62,4 +64,90 @@ describe("Workbook Manager tests", () => {
         expect(sharedStringIndex).toEqual(2);
         expect(newSharedStrings.replace(/ /g, "")).toContain(sharedStringsXmlMock.replace(/ /g, ""));
     });
+
+    // const updateWorkbookDataAndConfigurations = async (zip: JSZip, fileConfigs?: FileConfigs, tableData?: TableData, updateQueryTable = false): Promise<void> => {
+
+    test("Table XML contains correct Table reference value with headers included", async () => {
+        const singleTableDefaultTemplate = SIMPLE_BLANK_TABLE_TEMPLATE;
+
+        expect(async () => await JSZip.loadAsync(singleTableDefaultTemplate, {
+            base64: true,
+        })).not.toThrow();
+
+        const defaultZipFile = await JSZip.loadAsync(singleTableDefaultTemplate, {
+            base64: true,
+        });
+
+        const data = [
+            ["ID", "Name", "Income", "Gross", "Bonus"],
+            [123, "Alan C", 155000, 155000, 0.15],
+            [331, "Tim C", 65000, 13000, 0.12],
+            [222, "Bill G", 29501, 8850.3, 0.18],
+            [5582, "Mitch M", 87960, 17592, 0.15],
+            [43, "Dan F", 197296, 19729.6, 0.22],
+            [22, "Perry T-P", 186006, 37201.2, 0.4],
+            [335, "Mdrake", 197136, 78854.4, 0.1],
+            [6590, "Dr P", 139636, 41890.8, 0.13],
+        ];
+        const tableData = gridUtils.parseToTableData({ data: data, config: { promoteHeaders: true, adjustColumnNames: true } });
+
+        await xmlPartsUtils.updateWorkbookDataAndConfigurations(defaultZipFile, undefined, tableData);
+        expect(await defaultZipFile.file("xl/tables/table1.xml")?.async("text")).toContain("A1:E9");
+
+    });
+
+    test("Table XML contains correct Table reference value without headers included", async () => {
+        const singleTableDefaultTemplate = SIMPLE_BLANK_TABLE_TEMPLATE;
+
+        expect(async () => await JSZip.loadAsync(singleTableDefaultTemplate, {
+            base64: true,
+        })).not.toThrow();
+
+        const defaultZipFile = await JSZip.loadAsync(singleTableDefaultTemplate, {
+            base64: true,
+        });
+
+        const data = [
+            ["ID", "Name", "Income", "Gross", "Bonus"],
+            [123, "Alan C", 155000, 155000, 0.15],
+            [331, "Tim C", 65000, 13000, 0.12],
+            [222, "Bill G", 29501, 8850.3, 0.18],
+            [5582, "Mitch M", 87960, 17592, 0.15],
+            [43, "Dan F", 197296, 19729.6, 0.22],
+            [22, "Perry T-P", 186006, 37201.2, 0.4],
+            [335, "Mdrake", 197136, 78854.4, 0.1],
+            [6590, "Dr P", 139636, 41890.8, 0.13],
+        ];
+        const tableData = gridUtils.parseToTableData({ data: data, config: { promoteHeaders: false, adjustColumnNames: true } });
+
+        await xmlPartsUtils.updateWorkbookDataAndConfigurations(defaultZipFile, undefined, tableData);
+        expect(await defaultZipFile.file("xl/tables/table1.xml")?.async("text")).toContain("A1:E10");
+    });
+
+    test("Table XML contains correct Table reference value using template", async () => {
+        const movedTableDefaultTemplate = WORKBOOK_TEMPLATE_MOVED_TABLE;
+        expect(async () => await JSZip.loadAsync(movedTableDefaultTemplate, {
+            base64: true,
+        })).not.toThrow();
+
+        const templateMovedZipFile :any = await JSZip.loadAsync(movedTableDefaultTemplate, {
+            base64: true,
+        });
+        const data = [
+            ["ID", "Name", "Income", "Gross", "Bonus"],
+            [123, "Alan C", 155000, 155000, 0.15],
+            [331, "Tim C", 65000, 13000, 0.12],
+            [222, "Bill G", 29501, 8850.3, 0.18],
+            [5582, "Mitch M", 87960, 17592, 0.15],
+            [43, "Dan F", 197296, 19729.6, 0.22],
+            [22, "Perry T-P", 186006, 37201.2, 0.4],
+            [335, "Mdrake", 197136, 78854.4, 0.1],
+            [6590, "Dr P", 139636, 41890.8, 0.13],
+        ];
+        const tableData = gridUtils.parseToTableData({ data: data, config: { promoteHeaders: true, adjustColumnNames: true } });
+
+        await xmlPartsUtils.updateWorkbookDataAndConfigurations(templateMovedZipFile, { templateFile: templateMovedZipFile }, tableData);
+        expect(await templateMovedZipFile.file("xl/tables/table1.xml")?.async("text")).toContain("B2:F10");
+    });
 });
+
